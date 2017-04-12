5d3e3f0d47adc3bb797357300b4434c32f45077b
Nicolas Grenié <nicolas.grenie@gmail.com>
2017-04-12 12:34:34 +0200

    add activedocs command

--- commands/command-activedocs.js
+++ commands/command-activedocs.js
@@ -2,26 +2,54 @@ var activedocs = require("../lib/activedocs");
 
 module.exports = function activeDocsCommand(program) {
   program
-    .command("docs <cmd>")
-    .description("\n  create - Create a new method \n  list - List all methods of a service \n  show - Show a specific method of a service \n update - update a specific method of a service \n delete - Delete a method of a service")
+    .command("docs [command]")
+    .usage("docs <command> [options]")
+    .description("\n  create - Create a new ActiveDocs \n  list - List all ActiveDocs \n  update - Update a specific ActiveDocs spec \n  delete - Delete an ActiveDocs spec ")
     .option("-a, --docs <docs_id>","Specify activedocs id")
+    .option("-f, --file <path>", "Specify path to an API description file")
     .action(function(command,options){
       switch (command) {
         case "create":
-            break;
+          program.require(options.file,"Swagger spec file required");
+          activedocs.createActiveDocs(options.file).then(function(result){
+            var msg = "Activedocs "+result.name.inverse +" created. \n"
+            program.print({message:msg, type:"success", table: result, key:"api_doc","excludes":["body"]});
+          })
+          break;
         case "list":
-            activedocs.listActiveDocs().then(function(result){
-              var msg = "There are "+result.length+" activedocs spec on your account.\n"
-              program.print({message:msg, type:"success", table: result, key:"api_doc","excludes":["body"]});
-            })
-            break;
-        case "show":
+          activedocs.listActiveDocs().then(function(result){
+            var msg = "There are "+result.length+" activedocs spec on your account.\n"
+            program.print({message:msg, type:"success", table: result, key:"api_doc","excludes":["body"]});
+          })
+          break;
+        case "update":
+          program.require(options.docs,"ActiveDocs ID required");
+          program.require(options.file,"Swagger spec file required");
+          activedocs.updateActiveDocs(options.file, options.docs).then(function(result){
+            var msg = "ActiveDocs spec updated on your account.\n"
+            program.print({message:msg, type:"success", table: result, key:"api_doc","excludes":["body"]});
+          })
+          break;
             break;
         case "delete":
-            break;
+          program.require(options.docs,"ActiveDocs ID required");
+
+          activedocs.deleteActiveDocs(options.docs).then(function(result){
+            if(result){
+              var msg = "ActiveDocs id "+options.docs.inverse+" has been deleted."
+              program.print({message:msg, type:"success"});
+            }
+          })
+          break;
         default:
           program.error({message:"Unknown command \""+command+"\""});
           process.exit(1)
       }
     });
+
+    if(typeof command === "undefined"){
+      program.error({message:"Unknown command \""+command+"\""});
+      program.outputHelp()
+      process.exit(1)
+    }
 }
