c1569fb310e7d63ff42f24fe0b86cefb595f27be
Francesco Fiore <francescofiore.81@gmail.com>
2017-09-26 17:55:39 +0200

    Add applications command with list, show and create subcommands

---
+++ commands/command-applications.js
@@ -0,0 +1,53 @@
+var applications = require("../lib/applications");
+
+module.exports = function applicationsCommand(program) {
+  program
+    .command("applications <cmd>")
+    .description("\n  list - List all applications \n  show - Show a specific account")
+    .option("-a, --account <account_id>","Specify account id")
+    .option("-i, --id <application_id>","Specify application id")
+    .option("-p, --plan <plan_id>", "Specify application plain id")
+    .option("-n, --name <name>", "Specify application name")
+    .option("-d, --description <description>", "Specify application description")
+    .option("-k, --key <user_key>", "Specify the user-key")
+    .action(function(command, options){
+     program.isConfigured();
+
+      switch (command) {
+          case "list":
+            program.require(options.account,"Account ID");
+            applications.listApplications(options.account).then(function(result){
+              var msg = "There are "+result.length+" applications.\n"
+              program.print({message:msg, type:"success", table: result, key: "application"});
+            });
+            break;
+          case "show":
+            program.require(options.account,"Account ID");
+            program.require(options.id,"Application ID");
+            applications.showApplication(options.account, options.id).then(function(result){
+              var msg = "Details about application:\n"
+              program.print({message:msg, type:"success", data: result});
+            });
+            break;
+          case "create":
+            program.require(options.account, "Account ID");
+            program.require(options.plan, "Plan ID");
+            program.require(options.name, "Name");
+            program.require(options.description, "Description");
+            
+            var user_key = null
+            if (options.key) {
+              user_key = options.key;
+            }
+
+            applications.createApplication(options.account, options.plan, options.name, options.description, user_key).then(function(result){
+              var msg = "Application "+options.name+" created.\n"
+              program.print({message:msg, type:"success", data: result});
+            });
+            break;
+          default:
+            program.error({message:"Unknown command \""+command+"\""});
+            process.exit(1)
+      } //end switch
+    });
+}
